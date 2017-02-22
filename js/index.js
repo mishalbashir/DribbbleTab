63261ed45f8deda055fba94fb811d26a98082800
mishalbashir <mishaal.albashir@gmail.com>
2017-02-22 17:09:22 -0500

    Sorting Function
    Added functionality for sorting, but haven’t used it yet.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,36 @@
 var ItemsWidth = 100;
+perPage = 48;
 
+//sort items by various parameters
+/*
+function getSorted(selector, attrName) {
+    return $($(selector).toArray().sort(function(a, b){
+        var aVal = parseInt(a.getAttribute(attrName)),
+            bVal = parseInt(b.getAttribute(attrName));
+        return aVal - bVal;
+    }));
+}
+*/
+// function to get needed data from Dribbble
 $.ajax ({
   type:'GET',
   url: 'https://api.dribbble.com/v1/shots/?access_token=6c702277ce2fe998664aaf09b8359e2022b4ce809429a73f2bfe17b34205ca52',
+  data: {per_page: perPage},
   success: function (shots) {
-  //get shot urls for image population
-  $.each(shots, function(index, shot) {
-    console.log(shot.images.hidpi);
-    //  console.log(shot.images);
-      shotUrl = shot.html_url;
+
+    //get shot urls for image population
+    $.each(shots, function(index, shot) {
+        var shotViewsNumber = shot.views_count;
+        console.log(shotViewsNumber);
+        shotUrl = shot.html_url;
     //  console.log(shot.images.html_url);
-      shotView = shot.images.hidpi;
-      if (shotView == null) {
+        shotView = shot.images.hidpi;
+        if (shotView == null) {
         shotView = shot.images.normal;
       };
-//      $('#ItemArea').append('<img class="Items" src="' + shotView + '" />')
+
 //     This line gets a dribbble shot, creates a div for it, and then wraps it inside a clickable link
-      $('#ItemArea').append('<div class ="Items"> <a href ="' + shotUrl + '"> <img width= "' + ItemsWidth +'%" height = auto src="' + shotView + '" /> </a> </div>')
-//      $('.Items').wrapInner('<a href ="' + shotUrl + '"></a>')
+        $('#ItemArea').append('<div data-sort="' + shotViewsNumber + '" class ="Items"> <a href ="' + shotUrl + '"> <img width= "' + ItemsWidth +'%" height = auto src="' + shotView + '" /> </a> </div>')
     });
   }
 
